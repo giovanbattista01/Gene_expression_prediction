44aa66a1a4db7e0ae76761ab570919a1e8f700d6
giovanbattista01 <giovannilndln@gmail.com>
2024-10-26 17:38:43 +0200

    refactoring

--- cnns.py
+++ cnns.py
@@ -37,23 +37,36 @@ class ConvNetModel(nn.Module):
 class DeepConvNet(nn.Module):
     def __init__(self,width, nfeatures, filter_list, filtsize_list, padding_list, poolsize_list, n_states_linear1, n_states_linear2, noutputs):
         super(DeepConvNet, self).__init__()
-        self.conv1 = nn.Conv1d(nfeatures, filter_list[0], filtsize_list[0], padding=padding_list[0])
-        self.conv2 = nn.Conv1d(filter_list[0], filter_list[1], filtsize_list[1], padding=padding_list[1])
-        self.pool1 = nn.MaxPool1d(poolsize_list[0])
-        self.pool2 = nn.MaxPool1d(poolsize_list[1])
-        dim = int(width / (poolsize_list[0] * poolsize_list[1]) * filter_list[1])
-        self.fc1 = nn.Linear(dim, n_states_linear1)
-        self.fc2 = nn.Linear(n_states_linear1, n_states_linear2)
-        self.fc3 = nn.Linear(n_states_linear2, noutputs)
-        self.dropout = nn.Dropout(0.8) #0.5
+
+        assert(len(filter_list) == len(poolsize_list))
+
+        self.n_conv_layers = len(filter_list)
+        self.conv_layers = nn.ModuleList()
+        self.pools = nn.ModuleList()
+        for i in range(self.n_conv_layers):
+            if i == 0:
+                in_channels, out_channels  = nfeatures, filter_list[0]
+            else:
+                in_channels, out_channels  = filter_list[i-1], filter_list[i]
+            padding = padding_list[i]
+            kernel = filtsize_list[i]
+            self.conv_layers.append(nn.Conv1d(in_channels, out_channels, kernel, padding=padding))
+            self.pools.append(nn.MaxPool1d(poolsize_list[i]))
+
+            dim = int(width / math.prod(poolsize_list) * filter_list[1])
+            self.fc1 = nn.Linear(dim, n_states_linear1)
+            self.fc2 = nn.Linear(n_states_linear1, n_states_linear2)
+            self.fc3 = nn.Linear(n_states_linear2, noutputs)
+            self.dropout = nn.Dropout(0.8)
+
+        
 
     def forward(self, x):
-        x = self.conv1(x)
-        x = F.relu(x)
-        x = self.pool1(x)
-        x = self.conv2(x)
-        x = F.relu(x)
-        x = self.pool2(x)
+
+        for i in range(self.n_conv_layers):
+            x = self.conv_layers[i](x)
+            x = F.relu(x)
+            x = self.pools[i](x)
         x = x.view(x.size(0), -1)  # Flatten
         x = self.dropout(x)
         x = F.relu(self.fc1(x))
